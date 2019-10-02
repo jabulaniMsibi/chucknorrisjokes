6262e3d80eb83b1e20c777569fd7fe21b04d1042
jabulaniMsibi <47320264+jabulaniMsibi@users.noreply.github.com>
2019-10-02 15:12:12 +0200

    Initial Chuck Norris Commit

---
+++ ChuckNorrisJokesConsole/Program.cs
@@ -0,0 +1,20 @@
+﻿using ChuckNorrisJokesLibrary;
+using System;
+using System.Threading.Tasks;
+
+namespace ChuckNorrisJokesConsole
+{
+    class Program
+    {
+        static async Task  Main(string[] args)
+        {
+            JokeGenerator GenerateJoke = new JokeGenerator();
+            string randomJoke = await GenerateJoke.GetRandomJoke();
+            Console.WriteLine(randomJoke);
+
+            string[] categories = await GenerateJoke.GetCategories();
+            for (int index = categories.Length - 1; index >= 0; index--)
+                Console.WriteLine(categories[index]);
+        }
+    }
+}
